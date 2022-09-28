cc511ec26a2592297fefdf14c36d277300c04844
Chitranshu <chitranshuctr@gmail.com>
2022-09-29 05:08:02 +0530

    chitranshu

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Railway from './Component/Railway';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Railway/>
   </React.StrictMode>
 );
 
